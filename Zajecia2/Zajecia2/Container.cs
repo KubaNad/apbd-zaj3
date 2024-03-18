a9ba6881b720bf005013afeb9544cc405195299b
s29385 <s29385@pjwstk.edu.pl>
2024-03-18 13:27:45 +0100

    trzy

--- Zajecia2/Zajecia2/Container.cs
+++ Zajecia2/Zajecia2/Container.cs
@@ -1,7 +1,25 @@
 ﻿namespace Zajecia2
 {
-    public class Container
+    public abstract class Container
     {
-        
+        public string SerialNumber { get; }
+        public double LoadWeight { get; protected set; }
+        public double Height { get; }
+        public double OwnWeight { get; }
+        public double Depth { get; }
+        public double MaxLoadCapacity { get; }
+
+        protected Container(string serialNumber, double height, double ownWeight, double depth, double maxLoadCapacity)
+        {
+            SerialNumber = serialNumber;
+            Height = height;
+            OwnWeight = ownWeight;
+            Depth = depth;
+            MaxLoadCapacity = maxLoadCapacity;
+        }
+
+        public abstract void Load(double weight);
+        public void Unload() => LoadWeight = 0;
     }
+
 }
